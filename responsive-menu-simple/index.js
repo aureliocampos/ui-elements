73abd88e1ed1606d7ce47e6ce2e26914aaf832d0
Aurélio de Sousa Campos <aurelio.campos@mjv.net>
2023-04-07 21:21:42 -0300

    Iniciando projeto

---
+++ responsive-menu-simple/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let olaMundo = "Olá Mundo";
+const usuario2 = {
+    nome: "Hayes",
+    id: 10,
+};
+console.log(usuario2);
---
+++ responsive-menu-simple/index.ts
@@ -0,0 +1,13 @@
+let olaMundo = "Olá Mundo";
+
+const usuario2 = {
+  nome: "Hayes",
+  id: 10,
+};
+
+console.log(usuario2);
+
+interface Usuario {
+  nome: string;
+  id: number;
+}
